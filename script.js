e59a44335681c28262d0fbd5c7064ac53fdf3bf9
Semanta <semanta.lici@fshnstudent.info>
2024-04-05 10:56:13 +0200

    Final Commit

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-function roll(){
-    var RandomNumber1 = Math.floor(Math.random() * 6) + 1;
-    var randomDice1= "dice" + RandomNumber1 + ".png";
-    var diceImage1 = "images/" + randomDice1;
-    var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
-    var randomDice2 = "dice" + RandomNumber2 + ".png"
-    var diceImage2 = "images/" + randomDice2;
+function roll() {
+    // Get references to dice images
+    var image1 = document.querySelector(".img1");
+    var image2 = document.querySelector(".img2");
   
-    var image1= document.querySelectorAll("img")[0];
-    image1.setAttribute("src", diceImage1);
-
-    var image2=document.querySelectorAll("img")[1].setAttribute("src", diceImage2);
-
-    if(RandomNumber1 > RandomNumber2){
-        document.querySelector("h1").innerHTML = "Player 1 Wins!"
-    }
-    else{
-        document.querySelector("h1").innerHTML = "Player 2 Wins!"
-    }
-
-    var sum = RandomNumber1 + RandomNumber2;
-    var mult = RandomNumber1 * RandomNumber2;
-     
-    document.getElementById("results").innerHTML="Sum = " +sum + " Multiplication = " +mult;
-}
+    // Roll the dice animation
+    var rollCount = 0;
+    var rollInterval = setInterval(function () {
+      // Change dice images rapidly to simulate rolling
+      var RandomNumber1 = Math.floor(Math.random() * 6) + 1;
+      var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
+      image1.setAttribute("src", "images/dice" + RandomNumber1 + ".png");
+      image2.setAttribute("src", "images/dice" + RandomNumber2 + ".png");
+  
+      rollCount++;
+      // Stop animation after 10 iterations 
+      if (rollCount >= 10) {
+        clearInterval(rollInterval); // Stop the interval
+        // Determine the winner after animation stops
+        if (RandomNumber1 > RandomNumber2) {
+          document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        } else {
+          document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        }
+        // Display sum and multiplication results
+        var sum = RandomNumber1 + RandomNumber2;
+        var mult = RandomNumber1 * RandomNumber2;
+        document.getElementById("results").innerHTML =
+          "Sum = " + sum + " Multiplication = " + mult;
+      }
+    }, 100); // Change images every 100 milliseconds (adjust as needed)
+  }
+  
